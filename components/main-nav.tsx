1744755530b0126ba19e668eedc4b383e65975e5
mahmudul-hasan-hridoy <157973507+mahmudul-hasan-hridoy@users.noreply.github.com>
2024-01-29 10:56:43 +0000

    Styling

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import { cn } from "@/lib/utils"
-import CustomLink from "./custom-link"
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import CustomLink from "./custom-link";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -10,9 +10,9 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-} from "./ui/navigation-menu"
-import React from "react"
-import { Button } from "./ui/button"
+} from "./ui/navigation-menu";
+import React from "react";
+import { Button } from "./ui/button";
 
 export function MainNav() {
   return (
@@ -35,32 +35,32 @@ export function MainNav() {
         </NavigationMenuList>
       </NavigationMenu>
     </div>
-  )
+  );
 }
 
-const ListItem = React.forwardRef<HTMLLIElement, React.HTMLProps<HTMLLIElement>>(
-  ({ className, title, children, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={`
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={`
               block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors 
               dark:hover:bg-indigo-500 dark:hover:text-white dark:focus:bg-indigo-500 dark:focus:text-white
               hover:bg-indigo-100 hover:text-gray-900 focus:bg-indigo-100 focus:text-gray-900
               ${className}`}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="text-sm leading-snug line-clamp-2 dark:text-gray-400 text-gray-500">
-              {children}
-            </p>
-          </a>
-        </NavigationMenuLink>
-      </li>
-    );
-  }
-);
-
-ListItem.displayName = "ListItem";
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="text-sm leading-snug line-clamp-2 dark:text-gray-400 text-gray-500">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
