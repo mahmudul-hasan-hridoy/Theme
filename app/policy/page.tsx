bc13f3c2390b1876e0a3e8ab6e6fa3c509b1a592
mahmudul-hasan-hridoy <157973507+mahmudul-hasan-hridoy@users.noreply.github.com>
2024-01-29 09:14:53 +0000

    Modify

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -17,7 +17,7 @@ export default function PolicyPage() {
         <h2 className="text-xl font-bold">Privacy Policy</h2>
         <p>
           This site uses JSON Web Tokens and an in-memory database which resets
-          every ~2 hours.
+          every 2 hours.
         </p>
         <p>
           Data provided to this site is exclusively used to support signing in
